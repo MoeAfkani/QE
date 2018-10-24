86213230284504fcb09ea985b3880b101c2beb9b
Moonak <afkani.mohammad@gmail.com>
2018-10-24 09:54:47 +0330

    git

---
+++ grap-ecut.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "This is an output file for $0 excuted in $(date)" &> $1.dew
+##########################Constants##############################
+lineOftotEnergy=1
+lineOfP=1
+lineOfForce=1
+#################################################################
+  for ecutwfc in `LANG=en_US seq $2 $4 $3`
+    do
+    path=$(pwd)/$1/$ecutwfc
+    shitness=$(cat $path/$ecutwfc.$1.scf.out | grep -c "NOT")
+    if [ $shitness -eq 1 ]
+    then
+    # not conv
+      echo $ecutwfc | tee -a $1.dew
+    else 
+    # conv
+      let "lineOftotEnergy+=200"   #20 line upper than 1st time that "Energy" has observed! 
+      echo -n "$ecutwfc"| tee -a $1.dew ;tail -$lineOftotEnergy $path/$ecutwfc.$1.scf.out | grep "!    total energy" |tail -c 20 | tee -a $1.dew
+      eval "var$lineOftotEnergy=1";  # recovery to 1
+
+
+    fi 
+  done
+gnuplot -e "set terminal png size 1000,900; set output '$1.png'; plot[][]'$1.dew' u 1:2 with linespoints "
+
+eog $1.png &
